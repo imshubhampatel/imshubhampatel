cc3a492e40266c1d27ff67290fd6ce1ed2f6014b
Shubham Patel <75959964+imshubhampatel@users.noreply.github.com>
2024-09-07 17:08:26 +0530

    sdf

--- fetch_leetcode_stats.py
+++ fetch_leetcode_stats.py
@@ -6,18 +6,8 @@ url = "https://leetcard.jacoblin.cool/imshubhampatel?ext=activity"
 response = requests.get(url)
 
 if response.status_code == 200:
-    soup = BeautifulSoup(response.content, 'lxml')
-    
-    rect = soup.find('rect', id='background')
-    if rect:
-        rect['style'] = 'opacity: 0;'
-    
-    modified_svg = str(soup)
-
-    print(modified_svg[:1000]) 
-
     with open('leetcode_activity.svg', 'w') as file:
-        file.write(modified_svg)
+        file.write(response.content)
     print("SVG modified and saved successfully!")
 else:
     print(f"Failed to fetch SVG. Status code: {response.status_code}")
