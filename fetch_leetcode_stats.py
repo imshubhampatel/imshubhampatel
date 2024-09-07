f44effe128e07b0fc9feac7d70424eb29d15cbbd
Shubham Patel <75959964+imshubhampatel@users.noreply.github.com>
2024-09-07 16:44:08 +0530

    s

--- fetch_leetcode_stats.py
+++ fetch_leetcode_stats.py
@@ -10,23 +10,11 @@ response = requests.get(url)
 
 if response.status_code == 200:
     svg_content = response.content.decode('utf-8')
-    updated_style = '''
-#background {
-    transform: translate(0.5px, 0.5px);
-    stroke: var(--bg-2);
-    fill: var(--bg-0);
-    stroke-width: 1;
-    width: 499px;
-    height: 399px;
-    opacity: 0;
-    rx: 4px
-}
-'''
+    original_style = '#background {\n    transform: translate(0.5px, 0.5px);\n    stroke: var(--bg-2);\n    fill: var(--bg-0);\n    stroke-width: 1;\n    width: 499px;\n    height: 399px;\n    rx: 4px\n}\n'
+    new_style = '#background {\n    transform: translate(0.5px, 0.5px);\n    stroke: var(--bg-2);\n    fill: var(--bg-0);\n    stroke-width: 1;\n    width: 499px;\n    height: 399px;\n    opacity: 0;\n    rx: 4px\n}\n'
+    
 
-    svg_content = svg_content.replace(
-            '#background {\n    transform: translate(0.5px, 0.5px);\n    stroke: var(--bg-2);\n    fill: var(--bg-0);\n    stroke-width: 1;\n    width: 499px;\n    height: 399px;\n    rx: 4px\n}\n',
-            updated_style
-        )
+    svg_content = svg_content.replace(original_style,'#backgroud{}')
    
     with open('leetcode_activity.svg', 'w') as file:
         file.write(svg_content)
