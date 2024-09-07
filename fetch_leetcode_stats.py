6469927e6c3d504931dea975954d0ea8e1b945e5
Shubham Patel <75959964+imshubhampatel@users.noreply.github.com>
2024-09-07 16:34:21 +0530

    sdf

--- fetch_leetcode_stats.py
+++ fetch_leetcode_stats.py
@@ -7,24 +7,28 @@ url = "https://leetcard.jacoblin.cool/imshubhampatel?ext=activity"
 # Send a GET request to the URL
 response = requests.get(url)
 
-# Check if the request was successful
+
 if response.status_code == 200:
-     svg_content = response.content
-    
-    # Parse the SVG content
-    root = ET.fromstring(svg_content)
-    
-    # Modify the <rect> element with id="background"
-    for rect in root.findall(".//{http://www.w3.org/2000/svg}rect"):
-        if rect.get('id') == 'background':
-            rect.set('style', 'opacity: 0;')
-    
-    # Convert the modified content back to a string
-    modified_svg = ET.tostring(root, encoding='unicode')
-    
-    # Save the modified SVG to a file
+    updated_style = '''
+#background {
+    transform: translate(0.5px, 0.5px);
+    stroke: var(--bg-2);
+    fill: var(--bg-0);
+    stroke-width: 1;
+    width: 499px;
+    height: 399px;
+    opacity: 0;
+    rx: 4px
+}
+'''
+
+    svg_content = svg_content.replace(
+            '#background {\n    transform: translate(0.5px, 0.5px);\n    stroke: var(--bg-2);\n    fill: var(--bg-0);\n    stroke-width: 1;\n    width: 499px;\n    height: 399px;\n    rx: 4px\n}\n',
+            updated_style
+        )
+   
     with open('leetcode_activity.svg', 'w') as file:
-        file.write(modified_svg)
+        file.write(svg_content)
     print("SVG modified and saved successfully!")
 else:
     print(f"Failed to fetch SVG. Status code: {response.status_code}")
