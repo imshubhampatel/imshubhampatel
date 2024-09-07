b3243c70f8914b1bddb253c71bae381ba4283caa
Shubham Patel <75959964+imshubhampatel@users.noreply.github.com>
2024-09-07 16:21:40 +0530

    sd

--- fetch_leetcode_stats.py
+++ fetch_leetcode_stats.py
@@ -9,19 +9,20 @@ response = requests.get(url)
 
 # Check if the request was successful
 if response.status_code == 200:
-    # Parse the SVG content using BeautifulSoup
-    soup = BeautifulSoup(response.content, 'lxml')
+     svg_content = response.content
     
-    # Find the <rect> element with id="background"
-    rect = soup.find('rect', id='background')
-    if rect:
-        # Modify the style attribute
-        rect['style'] = 'opacity: 0;'
+    # Parse the SVG content
+    root = ET.fromstring(svg_content)
+    
+    # Modify the <rect> element with id="background"
+    for rect in root.findall(".//{http://www.w3.org/2000/svg}rect"):
+        if rect.get('id') == 'background':
+            rect.set('style', 'opacity: 0;')
     
     # Convert the modified content back to a string
-    modified_svg = str(soup)
-
-    # Open a file in write mode and save the modified content
+    modified_svg = ET.tostring(root, encoding='unicode')
+    
+    # Save the modified SVG to a file
     with open('leetcode_activity.svg', 'w') as file:
         file.write(modified_svg)
     print("SVG modified and saved successfully!")
