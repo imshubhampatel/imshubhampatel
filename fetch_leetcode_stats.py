b571171337a0dcb09f8842d5e40f0b4cbcc62d4c
Shubham Patel <75959964+imshubhampatel@users.noreply.github.com>
2024-09-07 17:54:26 +0530

    df

--- fetch_leetcode_stats.py
+++ fetch_leetcode_stats.py
@@ -6,8 +6,16 @@ url = "https://leetcard.jacoblin.cool/imshubhampatel?ext=activity"
 response = requests.get(url)
 
 if response.status_code == 200:
-    with open('leetcode_activity.svg', 'wb') as file:
-        file.write(response.content)
+    old_name_bytes =   '<rect id="background"></rect>'.encode('utf-8')
+    new_name_bytes =  '<rect id="background" style="opacity: 0;"></rect>'.encode('utf-8')
+    print(new_name_bytes)
+    print("kidfsd")
+    print(old_name_bytes)
+    with open('leetcode_activity.svg', 'rb') as file:
+        binary_data = file.read()
+        updated_binary_data = binary_data.replace(old_name_bytes, new_name_bytes)
+    with open("leetcode_activity.svg", 'wb') as file:
+        file.write(updated_binary_data)
     print("SVG modified and saved successfully!")
 else:
     print(f"Failed to fetch SVG. Status code: {response.status_code}")
